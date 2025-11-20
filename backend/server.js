837aff795c3e8b8e9defd2f7c8f91c4620f22787
AminaArslan <aminahameed369@gmail.com>
2025-11-20 16:28:20 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/" ,(req ,res) => res.send("API is running"))
 // Routes
 app.use("/api/boards", boardsRouter);
 app.use("/api/tasks", tasksRouter);
