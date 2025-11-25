0d17f3be83faad1b107d46c637e0732447c12069
AminaArslan <aminahameed369@gmail.com>
2025-11-25 11:17:54 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://task-manager-umber-mu.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
