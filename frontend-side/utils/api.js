e7e4e2ae2b453dbc2ee948b3e777c173f0215653
AminaArslan <aminahameed369@gmail.com>
2025-11-25 11:07:28 +0500

    YES

--- frontend-side/utils/api.js
+++ frontend-side/utils/api.js
@@ -3,7 +3,8 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 // ---- Boards ----
 export const fetchBoards = async () => {
   try {
-const res = await fetch(`${API_URL}/api/boards`);
+// const res = await fetch(`${API_URL}/api/boards`);
+const res = await fetch(`${API_URL}`);
     if (!res.ok) {
       const err = await res.json();
       throw new Error(err.error || "Failed to fetch boards");
