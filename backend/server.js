1f0d9fe5521cd4036869cc9b5caa70acf06af346
AminaArslan <aminahameed369@gmail.com>
2025-11-26 12:37:55 +0500

    new

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://task-manager-umber-mu.vercel.app"
+    // "https://task-manager-umber-mu.vercel.app"
+    "https://task-manager-umber-mu.vercel.app/authentication/login"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
