11676ffe6f3a09fe0800855ef1a9836b76ccb752
AminaArslan <aminahameed369@gmail.com>
2025-11-27 13:37:47 +0500

    update

--- frontend-side/app/api/axios.js
+++ frontend-side/app/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+    ? process.env.NEXT_PUBLIC_API_URL + "/api"
+    : "http://localhost:5000/api",
 });
-
 // Attach JWT token to all requests
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
